24a42e52392d24ff8a66cca61c79622e4c1e09fa
tofa <tofadze.1998@gmail.com>
2025-07-06 13:39:50 +0400

    creating transformer

--- data_pipeline/extract.py
+++ data_pipeline/extract.py
@@ -19,22 +19,82 @@ params = {'start_date': '2025-07-01',
           'end_date':'2025-07-02',
           'longitude': 44.8271,
           'latitude': 41.7151,
-          'hourly': ['temperature_2m' ,'weather_code'],
+          'hourly': ['temperature_2m' ,'weather_code', 'wind_speed_10m'],
           'timezone': 'GMT',
           'temporal_resolution': 'hourly_6'}
 
-#  ,'temporal_resolution': 'hourly_6'
-
-response = requests.get(base_url, params=params)
-response = response.json()
+#response = requests.get(base_url, params=params)
+#response = response.json()
 #pprint(type(response))
-pprint(response)
+#pprint(response)
 
-data_for_df = list(zip(response['hourly']['time'],
-                       response['hourly']['temperature_2m'],
-                       response['hourly']['weather_code']))
+# data_for_df = list(zip(response['hourly']['time'],
+#                        response['hourly']['temperature_2m'],
+#                        response['hourly']['weather_code']))
 
-schema = ["time", "temperature_c", "weather_code"]
+# schema = ["time", "temperature_c", "weather_code"]
 
-df = spark.createDataFrame(data_for_df, schema)
-df.show(3)
+# df = spark.createDataFrame(data_for_df, schema)
+# df.show(3)
+
+def extract_data(cities_info, cities, base_url, params, start_date, end_date):
+    
+    extracted_data = {}
+
+    for city in cities:
+        id = cities_info[city]['id']
+        lon = cities_info[city]['lon']
+        lat = cities_info[city]['lat']
+
+        params['lon'] = lon
+        params['lat'] = lat
+        params['start_date'] = start_date
+        params['end_date'] = end_date
+
+        r = requests.get(base_url, params=params)
+        r = r.json()
+
+        extracted_data[id] = r
+        
+    return extracted_data
+    
+extracted_data = extract_data(cities_info, cities, base_url, params,'2025-07-01', '2025-07-02')
+
+pprint(extracted_data)
+
+# transform ---------------------------------------------------------
+
+class DataTransformer:
+    def __init__(self, response_historic_data, cities, cities_info):
+        self.response_histiric_data = response_historic_data
+        self.cities = cities
+        self.cities_info = cities_info
+        self.dfs = []
+        self.transformed_df = 0
+
+    def create_historic_dataframes(self):
+        data = self.response_histiric_data
+        cities = self.cities
+        cities_info = self.cities_info 
+
+        for city in cities:
+            id = cities_info[city]
+
+            date = data[city]['hourly']['time']
+            temperature = data[city]['hourly']['temperature_2m']
+            weather_code = data[city]['hourly']['weather_code']
+
+            zipped_data_for_df = zip(date, temperature, weather_code)
+            schema = ["time", "temperature_c", "weather_code"]
+
+            df = spark.createDataFrame(zipped_data_for_df, schema)
+            df = df.withColumn('city_id', id)
+            
+            self.dfs.append(df)
+
+data_transformer = DataTransformer(extracted_data, cities, cities_info)
+data_transformer.create_historic_dataframes()
+
+data_transformer.dfs.show(3)
+
+            
