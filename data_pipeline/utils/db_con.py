015a6e6028e379289d75f90459481bd018075e89
tofa <tofadze.1998@gmail.com>
2025-10-28 21:46:21 +0400

    reworked db utils

--- data_pipeline/utils/db_con.py
+++ data_pipeline/utils/db_con.py
@@ -1,10 +1,26 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from dotenv import load_dotenv
+import os
 
 
 def establish_db_connection(
-    dbname="postgres", autocommit=False, isolation_level_autocommit=False, **db_config
+    dbname="postgres", autocommit=False, isolation_level_autocommit=False
 ):
+
+    load_dotenv()
+    HOST = os.getenv("HOST")
+    USER = os.getenv("USER")
+    PORT = os.getenv("PORT")
+    PASSWORD = os.getenv("PASSWORD")
+
+    db_config = {
+        "user": USER,
+        "password": PASSWORD,
+        "host": HOST,
+        "port": PORT
+    }
+
     conn = psycopg2.connect(**db_config, dbname=dbname)
 
     if isolation_level_autocommit:
--- data_pipeline/utils/get_engine.py
+++ data_pipeline/utils/get_engine.py
@@ -1,16 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy import URL
+from dotenv import load_dotenv
+import os
 
-def get_engine(host, user, password, db):
+
+def get_engine(db):
+
+    load_dotenv()
+    HOST = os.getenv("HOST")
+    USER = os.getenv("USER")
+    PASSWORD = os.getenv("PASSWORD")
 
     url_object = URL.create(
-    "postgresql+psycopg2",
-    username = user,
-    password = password,
-    host = host,
-    database = db,
+        "postgresql+psycopg2",
+        username=USER,
+        password=PASSWORD,
+        host=HOST,
+        database=db,
     )
 
     engine = create_engine(url_object)
-    
+
     return engine
